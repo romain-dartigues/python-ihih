0371d832b5017b80a9cf0f8c565fb0bc9d79c8cc
Romain Dartigues <romain.dartigues@gmail.com>
2015-02-13 11:12:12 +0100

    Bug: ReadTheDocs.org
    It seems around November 2013, Read The Docs changed their build engine
    and is trying to append extensions to "conf.py".

--- docs/conf.py
+++ docs/conf.py
@@ -25,10 +25,10 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = (
+extensions = [
 	'sphinx.ext.autodoc',
 	'sphinx.ext.intersphinx',
-)
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
